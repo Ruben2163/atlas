1275179ae220a80b2fdcdae510b5772ee8cda5f0
Ruben2163 <161133456+Ruben2163@users.noreply.github.com>
2026-08-03 11:18:45 +0100

    live temps v3

--- backend/stats.py
+++ backend/stats.py
@@ -3,6 +3,6 @@ from time import sleep
 
 
 for i in range(10):
-    cpu = open("/sys/class/thermal/thermal_zone0/temp")
-    print(cpu.read())
+    cpu = (open("/sys/class/thermal/thermal_zone0/temp")).read()
+    print(float(cpu)/1000)
     sleep(1)
